6da8c86817908580f2c0a20c8b81224b68bdaca2
Thammarith Likittheerameth <thammarith.likittheerameth@agoda.com>
2024-02-27 00:28:46 +0700

    Fix path

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import solidPlugin from 'vite-plugin-solid';
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
+  base: "/public-transport-timetable/",
   plugins: [
-    /* 
+    /*
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
