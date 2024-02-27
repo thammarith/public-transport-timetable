9dcc346b99c79919f9b7e3ec4c9b711ee1495bdf
Thammarith Likittheerameth <thammarith.likittheerameth@agoda.com>
2024-02-27 15:43:50 +0700

    Better UI

--- src/App.tsx
+++ src/App.tsx
@@ -8,39 +8,45 @@ const App: Component = () => {
     console.log(cuPopBus1);
 
     const [currentTime, setCurrentTime] = createSignal(new Date());
-    const [nextOne1, setNextOne1] = createSignal<number[]>([])
-    const [nextOne4, setNextOne4] = createSignal<number[]>([])
+    const [nextOne1, setNextOne1] = createSignal<number[]>([]);
+    const [nextOne4, setNextOne4] = createSignal<number[]>([]);
 
     setInterval(() => setCurrentTime(new Date()), 1000);
 
     createEffect(() => {
-        const getTime = (_: any) => _
-            .timetable[currentTime().getDay()]
-            ?.[currentTime().getHours()]
-            ?.frequencies
-            ?.filter((m: number) => m >= currentTime().getMinutes());
-
+        const getTime = (_: any) =>
+            _.timetable[currentTime().getDay()]?.[currentTime().getHours()]?.frequencies?.filter(
+                (m: number) => m >= currentTime().getMinutes()
+            );
 
         setNextOne1(getTime(cuPopBus1));
         setNextOne4(getTime(cuPopBus4));
     });
 
     return (
-        <section style={{ padding: '2rem'}}>
+        <section style={{ padding: '2rem' }}>
             <h1>
-                {currentTime().toLocaleDateString('de-de', {
+                {currentTime().toLocaleDateString('en-gb', {
                     dateStyle: 'full',
                 })}
                 <br />
-                {currentTime().toLocaleTimeString('de-de', {
+                {currentTime().toLocaleTimeString('en-gb', {
                     timeStyle: 'medium',
                 })}
             </h1>
             <h2>CU 1</h2>
-            <div>{JSON.stringify(nextOne1()) || 'NOT FOUND'}</div>
+            <div>
+                {nextOne1()
+                    .map((_) => `${currentTime().getHours()}:${_}`)
+                    .join(', ') || 'Not found'}
+            </div>
 
             <h2>CU 4</h2>
-            <div>{JSON.stringify(nextOne4()) || 'NOT FOUND'}</div>
+            <div>
+                {nextOne4()
+                    .map((_) => `${currentTime().getHours()}:${_}`)
+                    .join(', ') || 'Not found'}
+            </div>
         </section>
     );
 };
