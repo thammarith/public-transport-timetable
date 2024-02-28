60ae12e3c9dc05a3e3a008921a6bd9efc02c7335
Thammarith Likittheerameth <thammarith.likittheerameth@agoda.com>
2024-02-28 15:26:51 +0700

    fix naming

--- src/helpers/timetable.ts
+++ src/helpers/timetable.ts
@@ -4,9 +4,9 @@ export const _every = (frequency: number, start: number = 0, end: number = 59) =
     Array(60)
         .fill(0)
         .map((_, i) => i)
-        .filter((_) => _ % frequency === 0)
-        .map((_) => _ + start)
-        .filter((_) => start <= _ && _ <= end);
+        .filter((m) => m % frequency === 0)
+        .map((m) => m + start)
+        .filter((m) => start <= m && m <= end);
 
 export const every = (frequency: number, start: number = 0, end: number = 59) => build(_every(frequency, start, end));
 export const entries = build;
